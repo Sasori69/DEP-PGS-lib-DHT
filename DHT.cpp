0e75acdb85bcacd65ce63253b1381d212667f338
Sasori69 <sasori.spam@gmail.com>
2015-07-06 22:12:23 +0200

    merge

--- DHT.cpp
+++ DHT.cpp
@@ -6,11 +6,11 @@ written by Adafruit Industries
 
 #include "DHT.h"
 
-DHT::DHT(uint8_t pin, uint8_t count) {
+DHT::DHT(uint8_t pin) {
   _pin = pin;
-  _count = count;
-  firstreading = true;
-  readingok = false;
+  _firstreading = true;
+  // Note that count is now ignored as the DHT reading algorithm adjusts itself
+  // basd on the speed of the processor.
 }
 
 void DHT::begin(void) {
@@ -20,115 +20,157 @@ void DHT::begin(void) {
   _lastreadtime = 0;
 }
 
-//boolean S == Scale.  True == Farenheit; False == Celcius
+
 float DHT::readTemperature() {
-	float f;
+	float f = NAN;
+
 	if (read()) {
 		f = data[2] & 0x7F;
 		f *= 256;
 		f += data[3];
 		f /= 10;
-		if (data[2] & 0x80)
+		if (data[2] & 0x80) {
 			f *= -1;
-		return f;
+		}
 	}
-	return NAN;
+	return f;
 }
 
 float DHT::readHumidity(void) {
-	float f;
+	float f = NAN;
 	if (read()) {
 		f = data[0];
 		f *= 256;
 		f += data[1];
 		f /= 10;
-		return f;
 	}
-	return NAN;
+	return f;
 }
 
 boolean DHT::read(void) {
-  uint8_t laststate = HIGH;
-  uint8_t counter = 0;
-  uint8_t j = 0, i;
-  unsigned long currenttime;
-
   // Check if sensor was read less than two seconds ago and return early
   // to use last reading.
-  currenttime = millis();
+  uint32_t currenttime = millis();
   if (currenttime < _lastreadtime) {
     // ie there was a rollover
     _lastreadtime = 0;
   }
-  if (!firstreading && ((currenttime - _lastreadtime) < 2000)) {
-    return readingok; // return last correct measurement
-    //delay(2000 - (currenttime - _lastreadtime));
+  if (!_firstreading && ((currenttime - _lastreadtime) < 2000)) {
+    return _lastresult; // return last correct measurement
   }
-  firstreading = false;
-  readingok = false;
-  /*
-    Serial.print("Currtime: "); Serial.print(currenttime);
-    Serial.print(" Lasttime: "); Serial.print(_lastreadtime);
-  */
+  _firstreading = false;
   _lastreadtime = millis();
 
+  // Reset 40 bits of received data to zero.
   data[0] = data[1] = data[2] = data[3] = data[4] = 0;
   
-  // pull the pin high and wait 250 milliseconds
+  // Send start signal.  See DHT datasheet for full signal diagram:
+  //   http://www.adafruit.com/datasheets/Digital%20humidity%20and%20temperature%20sensor%20AM2302.pdf
+
+  // Go into high impedence state to let pull-up raise data line level and
+  // start the reading process.
   digitalWrite(_pin, HIGH);
   delay(250);
 
-  // now pull it low for ~20 milliseconds
+  // First set data line low for 20 milliseconds.
   pinMode(_pin, OUTPUT);
   digitalWrite(_pin, LOW);
   delay(20);
-  noInterrupts();
-  digitalWrite(_pin, HIGH);
-  delayMicroseconds(40);
-  pinMode(_pin, INPUT);
 
-  // read in timings
-  for ( i=0; i< MAXTIMINGS; i++) {
-    counter = 0;
-    while (digitalRead(_pin) == laststate) {
-      counter++;
-      delayMicroseconds(1);
-      if (counter == 255) {
-        break;
+  // Turn off interrupts temporarily because the next sections are timing critical
+  // and we don't want any interruptions.
+  {
+    InterruptLock lock;
+
+    // End the start signal by setting data line high for 40 microseconds.
+  	digitalWrite(_pin, HIGH);
+  	delayMicroseconds(40);
+
+    // Now start reading the data line to get the value from the DHT sensor.
+  	pinMode(_pin, INPUT);
+    delayMicroseconds(10);  // Delay a bit to let sensor pull data line low.
+
+// First expect a low signal for ~80 microseconds followed by a high signal
+    // for ~80 microseconds again.
+    if (expectPulse(LOW) == 0) {
+      DEBUG_PRINTLN(F("Timeout waiting for start signal low pulse."));
+      _lastresult = false;
+      return _lastresult;
+    }
+    if (expectPulse(HIGH) == 0) {
+      DEBUG_PRINTLN(F("Timeout waiting for start signal high pulse."));
+      _lastresult = false;
+      return _lastresult;
+    }
+
+    // Now read the 40 bits sent by the sensor.  Each bit is sent as a 50
+    // microsecond low pulse followed by a variable length high pulse.  If the
+    // high pulse is ~28 microseconds then it's a 0 and if it's ~70 microseconds
+    // then it's a 1.  We measure the cycle count of the initial 50us low pulse
+    // and use that to compare to the cycle count of the high pulse to determine
+    // if the bit is a 0 (high state cycle count < low state cycle count), or a
+    // 1 (high state cycle count > low state cycle count).
+    for (int i=0; i<40; ++i) {
+      uint32_t lowCycles = expectPulse(LOW);
+      if (lowCycles == 0) {
+        DEBUG_PRINTLN(F("Timeout waiting for bit low pulse."));
+        _lastresult = false;
+        return _lastresult;
       }
+      uint32_t highCycles = expectPulse(HIGH);
+      if (highCycles == 0) {
+        DEBUG_PRINTLN(F("Timeout waiting for bit high pulse."));
+        _lastresult = false;
+        return _lastresult;
+      }
+      data[i/8] <<= 1;
+      // Now compare the low and high cycle times to see if the bit is a 0 or 1.
+      if (highCycles > lowCycles) {
+        // High cycles are greater than 50us low cycle count, must be a 1.
+        data[i/8] |= 1;
+      }
+      // Else high cycles are less than (or equal to, a weird case) the 50us low
+      // cycle count so this must be a zero.  Nothing needs to be changed in the
+      // stored data.
     }
-    laststate = digitalRead(_pin);
-
-    if (counter == 255) break;
-
-    // ignore first 3 transitions
-    if ((i >= 4) && (i%2 == 0)) {
-      // shove each bit into the storage bytes
-      data[j/8] <<= 1;
-      if (counter > _count)
-        data[j/8] |= 1;
-      j++;
-    }
+    // Timing critical code is now complete.
 
   }
 
-  interrupts();
-  
-  /*
-  Serial.println(j, DEC);
-  Serial.print(data[0], HEX); Serial.print(", ");
-  Serial.print(data[1], HEX); Serial.print(", ");
-  Serial.print(data[2], HEX); Serial.print(", ");
-  Serial.print(data[3], HEX); Serial.print(", ");
-  Serial.print(data[4], HEX); Serial.print(" =? ");
-  Serial.println(data[0] + data[1] + data[2] + data[3], HEX);
-  */
+  DEBUG_PRINTLN(F("Received:"));
+  DEBUG_PRINT(data[0], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[1], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[2], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[3], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[4], HEX); DEBUG_PRINT(F(" =? "));
+  DEBUG_PRINTLN(data[0] + data[1] + data[2] + data[3], HEX);
 
-  // check we read 40 bits and that the checksum matches
-  if ((j >= 40) && 
-      (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
-    readingok = true;
+  // Check we read 40 bits and that the checksum matches.
+  if (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
+    _lastresult = true;
+    return _lastresult;
   }
-  
-  return readingok;
+  else {
+    DEBUG_PRINTLN(F("Checksum failure!"));
+    _lastresult = false;
+    return _lastresult;
+  }
+}
+
+// Expect the signal line to be at the specified level for a period of time and
+// return a count of loop cycles spent at that level (this cycle count can be
+// used to compare the relative time of two pulses).  If more than a millisecond
+// ellapses without the level changing then the call fails with a 0 response.
+uint32_t DHT::expectPulse(bool level) {
+  uint32_t count = 0;
+  uint32_t end = micros() + 1000;
+  // Loop while counting cycles until the level changes.
+  while (digitalRead(_pin) == level) {
+    count++;
+    if (micros() >= end) {
+      // Exceeded timeout waiting for level to change, fail.
+      return 0;
+    }
+  }
+  return count;
 }
